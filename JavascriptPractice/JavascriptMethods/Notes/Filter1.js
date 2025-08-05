7cdbc697db7bcb0a3d58288bd05a898cb6c43151
vipulmalik93 <malik.vipul93@gmail.com>
2025-08-06 00:12:00 +0530

    filter example added

---
+++ JavascriptPractice/JavascriptMethods/Notes/Filter1.js
@@ -0,0 +1,27 @@
+
+
+
+// M1
+const arr = [5, 1, 3, 2 ,6];
+
+
+function isOdd (x) {
+    return x % 2;
+}
+
+const filter1 = arr.filter(isOdd);
+console.warn("filter1:", filter1);
+
+
+// M2
+
+const filter2 = arr.filter(function greaterThanFour (x) {
+      return x  > 4;
+})
+console.warn("filter2:", filter2);
+
+
+// M3
+
+const filter3 = arr.filter((x) => x > 4);
+console.warn("filter3:", filter3);
