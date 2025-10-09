694b00c074e1f2466903f2b8e636b48ccc3d3785
vipulmalik93 <malik.vipul93@gmail.com>
2025-10-09 17:56:25 +0530

    classes example added

--- JavascriptPractice/Classes/Classes1.js
+++ JavascriptPractice/Classes/Classes1.js
@@ -1,5 +1,7 @@
 class Person {
     city = "jaipur";
+    // let country = "india";     
+    //  the above country line   will  give error because we can not write 
     constructor (name) {
         this.name = name;
     }
@@ -11,5 +13,15 @@ class Person {
 }
 
 const person1 = new Person("vipul")
+console.log("person1-1:", person1);
 person1.sayHello()
 console.log("CITY:", person1.city);
+person1.name = "monty";
+person1.state = "westbangal"
+person1.city = "meerut";
+console.log("person1-2:", person1)
+const person2 = new Person("rahul");
+console.log("person2:", person2)
+person2.sayHello();
+const person3 = new Person();
+console.log("person3:", person3);
