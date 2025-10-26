0b84c2dca303f2381712ae04a7dc84a4bd3df9bc
vipulmalik93 <malik.vipul93@gmail.com>
2025-10-27 01:17:52 +0530

    shadow more added

--- JavascriptPractice/Scope/Questions/Shadow3.1.js
+++ JavascriptPractice/Scope/Questions/Shadow3.1.js
@@ -62,3 +62,21 @@ console.log(abc1)
  // EXAMPLE2:
 //  20
 //  123
+
+
+
+// Explanation:
+// Explanation of example2:
+
+// when we pass through parameters and than that parameter creates localvariable there in that scope see after 
+// applying debugger in code that that variable is present in the local scope of function so assiging new value 
+// to that variable which is passed through parameter won't affect the global variable value because in this case 
+// a local variable of that name of which parameter is is created and that one is updated not the global one.
+
+// 1. The parameter abc creates a local variable inside the function test.
+// 2. Inside the function, when you write abc = 20, it reassigns the local variable, not the global one.
+// 3. Then it logs 20.
+
+// In short:
+// Function parameters create local copies of the values you pass.
+// Changing the parameter does not affect the original global variable if the argument is a primitive type (like number, string, boolean).
