a3bad804504c1c5d072483d1aba7c04314442b38
vipulmalik93 <malik.vipul93@gmail.com>
2025-11-20 09:06:21 +0530

    map completed

--- JavascriptPractice/JavascriptMethods/Notes/Map/MapPractice/Map4.js
+++ JavascriptPractice/JavascriptMethods/Notes/Map/MapPractice/Map4.js
@@ -41,6 +41,41 @@ const user = [
 
             // item.isAdult = true → returns true, that’s why newUsers is [true, true].
 
+// ------------------------------------------------------------------------------------------------------------------------
+
+            // explanation with example................
+
+            // const user = { name: "Vipul" };
+            // const result = (user.isAdult = true);
+            // console.log(result);
+            // console.log(user);
+
+
+
+            // OUTPUT:
+                        // true
+                        // { name: 'Vipul', isAdult: true }    
+
+    //   const result = (user.isAdult = true) for this line what is happening is that we are using assignment and in assignment value is returned.
+
+
+            //  SUPER SIMPLE SUMMARY
+                    // isAdult: true
+
+                    // ➡ Only used inside { }
+                    // ➡ Just defines a property
+                    // ➡ Does NOT run code
+                    // ➡ Does NOT return a value
+
+                    // isAdult = true
+
+                    // ➡ Assignment statement
+                    // ➡ Mutates object
+                    // ➡ Returns the assigned value (true)
+                    // ➡ Can be used anywhere
+
+// ------------------------------------------------------------------------------------------------------------------------
+
 
 
 
